9cb2e39de4b49b73a5b4633538aaa36bfa2098a0
Aziz <aziztoulgui20@gmail.com>
2025-03-02 14:16:22 +0100

    Animating the vehicles

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
-import {Render} from "./components/Renderer";
-import {Camera} from "./components/Camera";
-import {player} from "./components/Player";
-import {map, initializeMap} from "./components/Map";
-import './style.css';
-import {DirectionalLight} from "./components/DirectionalLight.js";
-
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { DirectionalLight } from "./components/DirectionalLight";
+import { player } from "./components/Player";
+import { map, initializeMap } from "./components/Map";
+import { animateVehicles } from "./animateVehicles";
+import "./style.css";
 
 const scene = new THREE.Scene();
 scene.add(player);
@@ -14,18 +14,24 @@ scene.add(map);
 const ambientLight = new THREE.AmbientLight();
 scene.add(ambientLight);
 
-const dirlight = DirectionalLight();
-scene.add(dirlight);
-
+const dirLight = DirectionalLight();
+scene.add(dirLight);
 
 const camera = Camera();
 scene.add(camera);
 
 initializeGame();
 
-function initializeGame(){
+function initializeGame() {
     initializeMap();
 }
 
-const renderer = Render();
+const renderer = Renderer();
 renderer.render(scene, camera);
+renderer.setAnimationLoop(animate);
+
+function animate() {
+    animateVehicles();
+
+    renderer.render(scene, camera);
+}
